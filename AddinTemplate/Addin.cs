789fdda447e1e000ffe2fe10d8716f5e058e5788
Rune Tvilum <rune@addin.dk>
2017-10-05 13:32:30 +0200

    initial
    closes #1

---
+++ AddinTemplate/Addin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace $safeprojectname$
+{
+    [Bentley.MicroStation.AddInAttribute(MdlTaskID = "$safeprojectname$", KeyinTree = "$safeprojectname$.Commands.xml")]
+    public class Addin : Bentley.MicroStation.AddIn
+    {
+        internal static Bentley.MicroStation.AddIn MainAddin { get; set; }
+
+        private Addin(System.IntPtr mdlDesc)
+            : base(mdlDesc)
+        {
+            MainAddin = this;
+        }
+
+        protected override int Run(string[] commandLine)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AddinTemplate/KeyinCommands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Application = Bentley.Interop.MicroStationDGN.Application;
+
+namespace $safeprojectname$
+{
+    public static class KeyinCommands
+    {
+        public static void Start(System.String unparsed)
+        {
+            Application comApp = Bentley.MicroStation.InteropServices.Utilities.ComApp;
+
+            MessageBox.Show("Start-keyin called.");
+
+        }
+
+        public static void Debug(System.String unparsed)
+        {
+            Application comApp = Bentley.MicroStation.InteropServices.Utilities.ComApp;
+
+            MessageBox.Show("Debug-keyin called.");
+        }
+    }
+}
